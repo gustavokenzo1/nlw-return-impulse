0d650bb6e0d0d38443746e19caad8a93ce3b7fc0
gustavokenzo1 <gustavokenzo314@gmail.com>
2022-05-14 22:47:48 -0300

    build

--- server/dist/server.js
+++ server/dist/server.js
@@ -5,11 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const routes_1 = require("./routes");
+const router_1 = __importDefault(require("./routes/router"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-app.use(routes_1.routes);
-app.listen(3333, () => {
+app.use(express_1.default.json({
+    limit: "10mb",
+}));
+app.use(router_1.default);
+app.listen(process.env.PORT || 3333, () => {
     console.log("Server started on port 3333!");
 });
